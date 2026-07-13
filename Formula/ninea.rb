0f19cc738dc3d020014c5210c238cd02789e8882
LPX-E5BD8 <lipegnxiang_@outlook.com>
2026-07-13 21:43:41 +0800

    Update ninea to 0.2.5

--- Formula/ninea.rb
+++ Formula/ninea.rb
@@ -1,26 +1,26 @@
 class Ninea < Formula
   desc "Expose MCP, A2A, and HTTP capabilities as filesystem-native skills"
   homepage "https://github.com/gopact-ai/9a"
-  version "0.2.4"
+  version "0.2.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/gopact-ai/9a/releases/download/v0.2.4/9a_0.2.4_darwin_arm64.tar.gz"
-      sha256 "7f646c8bae6dddeba04c5da4ba47320861e6f8d54f2bce0268f36803ff8a1b63"
+      url "https://github.com/gopact-ai/9a/releases/download/v0.2.5/9a_0.2.5_darwin_arm64.tar.gz"
+      sha256 "648acffeea2bef35801aeb9a42003af16bbf78ee5ca7880226c34a6d0440b046"
     else
-      url "https://github.com/gopact-ai/9a/releases/download/v0.2.4/9a_0.2.4_darwin_amd64.tar.gz"
-      sha256 "f72876839c4989268054f36a315b084009c3cc76098de5abba6f38120bf57444"
+      url "https://github.com/gopact-ai/9a/releases/download/v0.2.5/9a_0.2.5_darwin_amd64.tar.gz"
+      sha256 "cca6d11607a41e2ecc6d0294b356ffd1f8ec46a254536d9c3ae625db0e4cf5c3"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/gopact-ai/9a/releases/download/v0.2.4/9a_0.2.4_linux_arm64.tar.gz"
-      sha256 "c1e27bc3de52e021f591013bbc654ce12f3ef2f0b246063232e2dff745749d42"
+      url "https://github.com/gopact-ai/9a/releases/download/v0.2.5/9a_0.2.5_linux_arm64.tar.gz"
+      sha256 "20eacf54193d33ca73e6291a7cc57ff4b8996e5c0905e83e36c9658b8544a8b2"
     else
-      url "https://github.com/gopact-ai/9a/releases/download/v0.2.4/9a_0.2.4_linux_amd64.tar.gz"
-      sha256 "93c0970b6bbc9e26cbcb5e01451c5ed5cfee48dab734869149d434abdee5542f"
+      url "https://github.com/gopact-ai/9a/releases/download/v0.2.5/9a_0.2.5_linux_amd64.tar.gz"
+      sha256 "8fa7a82ff69375d23bf750de7be7066d91d774103ce56b58fb35f9349bb00add"
     end
   end
 
